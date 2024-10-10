1264c758fcdeb24f6d4fdda1b5ef6e370c9162c6
Mariusz Fik <mariusz@fidano.pl>
2024-10-10 23:25:47 +0200

    Create opentofu s3 bucket by default.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- modules/nas-s3/main.tf
+++ modules/nas-s3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "s3" {
-  for_each = toset(var.buckets)
+  for_each = toset(concat(var.buckets, ["opentofu"]))
 
-  bucket = each.value
-  force_destroy = true  
+  bucket        = each.value
+  force_destroy = true
 }
