64f75ceb73380358956ca4a0ad6026cf75f78692
Mariusz Fik <mariusz@fidano.pl>
2026-02-23 00:07:14 +0100

    Configure S3 backend for state storage
    Set up S3 state storage with configurations optimized for Garage,
    including path-style access and lockfile support.
    
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ terraform/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket   = "opentofu-state"
+    key      = "main.tfstate"
+    region   = "garage"
+    endpoint = var.s3_endpoint
+    # access_key = "" # AWS_ACCESS_KEY_ID
+    # secret_key = "" # AWS_SECRET_ACCESS_KEY
+
+    # Required for non-AWS / self-hosted S3
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+
+    # Optional but good
+    encrypt      = false # Garage SSE support is limited — keep false unless tested
+    use_lockfile = true  # Enables per-state locking (very useful!)
+  }
+}
